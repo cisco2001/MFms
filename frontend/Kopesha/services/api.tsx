7bfb5da44f6296914dee57f592806e5aa4bc5476
Mtwa Mgimwa <mtwa.mgimwa@nokturntech.co.tz>
2025-04-25 00:25:49 +0300

    i don't know what added but needs to be removed

--- frontend/Kopesha/services/api.tsx
+++ frontend/Kopesha/services/api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://192.168.183.10:8000/api/';
+const API_BASE_URL = 'http://192.168.0.34:8000/api/';
 
 
 export const submitLoanApplication = async (formData: FormData, token: string) => {
